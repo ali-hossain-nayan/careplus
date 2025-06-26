ef2fc6657b71aa4db5791a9a29f20fdfd02a0cd2
Nayan1234l <ali15-13943@diu.edu.bd>
2025-06-26 13:48:44 +0600

    update-success-page-hotfix

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -2,78 +2,78 @@ import { Button } from '@/components/ui/button';
 import { Doctors } from '@/constants';
 import { getAppointment } from '@/lib/actions/appointment.actions';
 import { formatDateTime } from '@/lib/utils';
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 const Success = async ({ params: { userId }, searchParams }: SearchParamProps) => {
+  const appointmentId = (searchParams?.appointmentId as string) || '';
+  const appointment = await getAppointment(appointmentId);
+  const doctor = Doctors.find((doc) => doc.name === appointment.primaryPhysician);
 
-    //take appointment to id to show the appointment details
-    const appointmentId = (searchParams?.appointmentId as string || "");
-    const appointment = await getAppointment(appointmentId)
-    const doctor = Doctors.find((doctor) => doctor.name === appointment.primaryPhysician)
-    return (
-        <div className='flex h-screen max-h-screen px-[5%]'>
-            <div className="success-img">
-                <Link href='/'>
-                    <Image
-                        src='/assets/icons/logo-full.svg'
-                        height={1000}
-                        width={1000}
-                        alt='logo'
-                        className='h-10 w-fit cursor-pointer'
-                    />
-                </Link>
+  return (
+    <div className="flex h-screen max-h-screen px-[5%]">
+      <div className="success-img">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="logo"
+            className="h-10 w-fit cursor-pointer"
+          />
+        </Link>
 
-                <section className='flex flex-col items-center'>
-                    <Image
-                        src='/assets/gifs/success.gif'
-                        height={300}
-                        width={400}
-                        alt='success'
-                    />
-                    <h2 className='header mb-6 max-w-[600px] text-center'>Your <span className='text-green-500'>appointment request</span> has been successfully submitted.</h2>
-                    <p>We will be inform in a short period to confirm</p>
-                </section>
+        <section className="flex flex-col items-center">
+          <Image
+            src="/assets/gifs/success.gif"
+            height={300}
+            width={400}
+            alt="success"
+          />
+          <h2 className="header mb-6 max-w-[600px] text-center">
+            Your <span className="text-green-500">appointment request</span> has been successfully submitted.
+          </h2>
+          <p>We will inform you shortly to confirm.</p>
+        </section>
 
-                <section className="request-details">
-                    <p>Requested appointment details:</p>
-                    {doctor ? (
-                        <div className="flex items-center gap-3">
-                            <Image
-                                src={doctor.image}
-                                width={100}
-                                height={100}
-                                className="size-6"
-                                alt="doctor"
-                            />
-                            <p className="whitespace-nowrap">Dr. {doctor.name}</p>
-                        </div>
-                    ) : (
-                        <p className="text-red-500">Doctor not found</p>
-                    )}
-                    <div className="flex gap-2">
-                        <Image
-                            src="/assets/icons/calendar.svg"
-                            height={24}
-                            width={24}
-                            alt="calendar"
-                        />
-                        <p>{formatDateTime(appointment.schedule).dateTime}</p>
-                    </div>
-                </section>
+        <section className="request-details">
+          <p>Requested appointment details:</p>
 
-
-                <Button variant="outline" className='shad-primary-btn' asChild>
-                    <Link href={`/patients/${userId}/new-appointment`}>New Appointment
-
-                    </Link>
-                </Button>
-                <p className='copyright mt-10 py-12'>
-                    &copy; 2024 MediCare</p>
+          {doctor ? (
+            <div className="flex items-center gap-3">
+              <Image
+                src={doctor.image}
+                width={100}
+                height={100}
+                className="size-6"
+                alt="doctor"
+              />
+              <p className="whitespace-nowrap">Dr. {doctor.name}</p>
             </div>
-        </div>
-    )
-}
+          ) : (
+            <p className="text-red-500">Doctor not found</p>
+          )}
 
-export default Success
+          <div className="flex gap-2">
+            <Image
+              src="/assets/icons/calendar.svg"
+              height={24}
+              width={24}
+              alt="calendar"
+            />
+            <p>{formatDateTime(appointment.schedule).dateTime}</p>
+          </div>
+        </section>
+
+        <Button variant="outline" className="shad-primary-btn" asChild>
+          <Link href={`/patients/${userId}/new-appointment`}>New Appointment</Link>
+        </Button>
+
+        <p className="copyright mt-10 py-12">&copy; 2024 MediCare</p>
+      </div>
+    </div>
+  );
+};
+
+export default Success;
