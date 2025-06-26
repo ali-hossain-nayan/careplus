988b5744cb068a7db078eb5eccf4e69c922978f8
Nayan1234l <ali15-13943@diu.edu.bd>
2025-06-26 13:28:19 +0600

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,19 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { cn } from "@/lib/utils"
 import { ThemeProvider } from "next-themes";
-import { cn } from "@/lib/utils";
 
-// Register local fonts and assign to CSS variables
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-  display: "swap",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-  display: "swap",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "MediCare",
@@ -25,13 +22,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{  
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body className={cn("min-h-screen bg-dark-300 font-sans antialiased")}>
-        <ThemeProvider attribute="class" defaultTheme="dark">
+    <html lang="en"> 
+      <body
+        className={cn('min-h-screen bg-dark-300 font-sans antialiased')}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+        >
           {children}
         </ThemeProvider>
       </body>
