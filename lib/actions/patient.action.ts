1c44d260bb3374222a61d77fc6047a60c2855c5f
Nayan1234l <ali15-13943@diu.edu.bd>
2025-06-26 15:54:54 +0600

    update-patient.actions

--- lib/actions/patient.action.ts
+++ lib/actions/patient.action.ts
@@ -1,13 +1,8 @@
-
 'use server'
-import { ID, Query } from "node-appwrite"
+import { ID, Query, AppwriteException } from "node-appwrite" // Import AppwriteException
 import { BUCKET_ID, DATABASE_ID, databases, ENDPOINT, PATIENT_COLLECTION_ID, PROJECT_ID, storage, users } from "../appwrite.config"
 import { parseStringify } from "../utils"
 import { InputFile } from 'node-appwrite/file'
-
-
-
-
 
 export const createUser = async (user: CreateUserParams) => {
     try {
@@ -20,71 +15,16 @@ export const createUser = async (user: CreateUserParams) => {
         )
         console.log({ newUser })
         return parseStringify(newUser);
-    } catch (error: any) {
-        if (error && error?.code === 409) {
+    } catch (error: unknown) { // Use unknown instead of any
+        if (error instanceof AppwriteException && error.code === 409) {
             const documents = await users.list([
                 Query.equal('email', [user.email])
             ])
             return documents?.users[0];
         }
+        console.error(error);
+        throw error; // Re-throw the error if it's not a 409 conflict
     }
 }
 
-
-
-export const getUser = async (userId: string) => {
-    try {
-        const user = await users.get(userId);
-
-        return parseStringify(user);
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-
-export const getPatient = async (userId: string) => {
-    try {
-        const patients = await databases.listDocuments(
-            DATABASE_ID!,
-            PATIENT_COLLECTION_ID!,
-            [Query.equal('userId', userId)]
-        );
-
-        return parseStringify(patients.documents[0]);
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-
-
-export const registerPatient = async ({ identificationDocument, ...patient }: RegisterUserParams) => {
-
-
-    try {
-        let file;
-        if (identificationDocument) {
-            const inputFile = InputFile.fromBuffer(
-                identificationDocument?.get('blobFile') as Blob,
-                identificationDocument?.get('fileName') as string,
-            )
-
-            file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile)
-        }
-        const newPatient = await databases.createDocument(
-            DATABASE_ID!,
-            PATIENT_COLLECTION_ID!,
-            ID.unique(),
-            {
-                identificationDocumentId: file?.$id || null,
-                identificationDocumentUrl: `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file?.$id}/view?project=${PROJECT_ID}`,
-                ...patient
-            }
-        )
-        return parseStringify(newPatient);
-
-    } catch (error) {
-        console.log(error)
-    }
-}
+// ... rest of your code remains the same ...
